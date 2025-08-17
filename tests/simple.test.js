8a5be561e882b084bec7ab84eaf3ae285cda40b7
Adrian Roß <adrian.ross@ross-agentur.de>
2025-08-17 12:26:47 +0200

    Implement simple testsuite

--- tests/simple.test.js
+++ tests/simple.test.js
@@ -19,3 +19,78 @@ test('Simple div', () => {
     '<div class="container simple-container">hello world!</div>',
   );
 });
+
+test(`Simple replacement`, () => {
+  var div = {
+    type: 'div',
+    class: '${classname}',
+    innerHTML: '${content}',
+  };
+
+  var args = {
+    classname: 'container',
+    content: 'Hello World!',
+  };
+
+  var html = visit(
+    div,
+    () => {
+      return {};
+    },
+    args,
+  );
+
+  expect(html).toContain('<div class="container">Hello World!</div>');
+});
+
+test(`Charset meta`, () => {
+  var meta = {
+    type: 'meta',
+    charset: 'UTF-8',
+  };
+
+  var html = visit(
+    meta,
+    () => {
+      return {};
+    },
+    {},
+  );
+
+  expect(html).toContain('<meta charset="UTF-8" />');
+});
+
+test(`<p> tag`, () => {
+  var e = {
+    type: 'p',
+    innerHTML: 'hello, world',
+  };
+
+  var html = visit(
+    e,
+    () => {
+      return {};
+    },
+    {},
+  );
+
+  expect(html).toContain('<p>hello, world</p>');
+});
+
+test(`Stylesheet`, () => {
+  var style = {
+    type: 'link',
+    rel: 'stylesheet',
+    href: 'style.css',
+  };
+
+  var html = visit(
+    style,
+    () => {
+      return {};
+    },
+    {},
+  );
+
+  expect(html).toContain('<link rel="stylesheet" href="style.css" />');
+});
